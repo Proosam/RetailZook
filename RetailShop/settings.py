2e628f5637f493aaa88139b9f6999ba643dad3a5
osama ahmed <osama1000024@gmail.com  git config --global user.name osama>
2021-02-17 11:10:31 +0530

    whitenoise

--- RetailShop/settings.py
+++ RetailShop/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
